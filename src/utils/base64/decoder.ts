85f7a4d6ce35641cd80ba8a2433955ea2f902ec6
sibmaks <sibmaks@vk.com>
2025-03-22 18:37:18 +0300

    Added method text2array into Base64.Decoder

--- src/utils/base64/decoder.ts
+++ src/utils/base64/decoder.ts
@@ -1,11 +1,11 @@
 const decoder = new TextDecoder();
 
 /**
- * Decode Base64 string to array buffer
+ * Decode Base64 string to array
  * @param base64 base64 string
- * @returns decoded array buffer
+ * @returns decoded array
  */
-export const text2buffer = (base64: string): ArrayBuffer => {
+export const text2array = (base64: string): Uint8Array => {
   const binaryString = atob(base64);
   const len = binaryString.length;
   const bytes = new Uint8Array(len);
@@ -14,6 +14,16 @@ export const text2buffer = (base64: string): ArrayBuffer => {
     bytes[i] = binaryString.charCodeAt(i);
   }
 
+  return bytes;
+};
+
+/**
+ * Decode Base64 string to array buffer
+ * @param base64 base64 string
+ * @returns decoded array buffer
+ */
+export const text2buffer = (base64: string): ArrayBuffer => {
+  const bytes = text2array(base64);
   return bytes.buffer;
 };
 
